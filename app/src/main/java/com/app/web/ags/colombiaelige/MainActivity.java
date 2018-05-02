11844349cb311919205e73a544f7bfc145d61a35
alejogs4 <alejogs4@gmail.com>
2018-05-01 23:07:07 -0500

    Add init files

---
+++ app/src/main/java/com/app/web/ags/colombiaelige/MainActivity.java
@@ -0,0 +1,15 @@
+package com.app.web.ags.colombiaelige;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
